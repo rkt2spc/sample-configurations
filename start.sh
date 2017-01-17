be8e2b3ded389c698d7373e4d9aac03feaf3f0ee
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-01-18 06:56:43 +0700

    Hey

--- start.sh
+++ start.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
 #----------------------------------------------------------------------------
-# Start MongoDB
-# service mongod start
+# Arguments
+domain='bn-gateway.nmtuan.me'
+
+#----------------------------------------------------------------------------
+# Extract Arguments
+for flag in "$@"
+do
+    case $flag in
+        -d=*|--domain=*)
+            domain="${flag#*=}"
+            shift
+            ;;
+        *)
+            echo "Unexpected option ${flag}, option have no meaning"
+            ;;
+    esac
+done
 
 #----------------------------------------------------------------------------
 # Start letsencrypt
-letsencrypt 
-letsencrypt certonly --standalone
+letsencrypt certonly --standalone -d "${domain}"
 
 #----------------------------------------------------------------------------
 # Start NGINX
 cat nginx.conf | tee /etc/nginx/sites-available/default
+sed -i -- "s/your_domain_name/${domain}/g" /etc/nginx/sites-available/default
 nginx
